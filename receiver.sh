6c0704e5b60a26021f3521e908b090b9fc88f734
Philipp Gaßner <activator112233@gmail.com>
2025-05-08 18:47:52 +0200

    fixed logging scripts

--- receiver.sh
+++ receiver.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-RUST_BACKTRACE=1 cargo run --bin receiver -- -d "MacBook Pro Speakers" -c 0 -c 1 | tee receiver.log
+DATE=$(date +"%Y-%m-%dT%H:%M:%S")
+mkdir -p logs
+RUST_BACKTRACE=1 cargo run --bin receiver -- -d "MacBook Pro Speakers" -c 0 -c 1  2>&1 | tee logs/receiver.$DATE.log
--- sender.sh
+++ sender.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-RUST_BACKTRACE=1 cargo run --bin sender -- -n 10.0.0.41:42069 -d "BlackHole 64ch" -c 0 -c 1 | tee sender.log
+DATE=$(date +"%Y-%m-%dT%H:%M:%S")
+mkdir -p logs
+RUST_BACKTRACE=1 cargo run --bin sender -- -n 10.0.0.41:42069 -d "BlackHole 64ch" -c 0 -c 1 2>&1 | tee logs/sender.$DATE.log
